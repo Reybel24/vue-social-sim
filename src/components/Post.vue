307033889542894c1e393e3829147cd0d9ffa30b
reybel24 <reybel24@gmail.com>
2020-03-04 23:40:28 -0500

    add post comment area

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -7,6 +7,7 @@
       </div>
       <div class="text user-name">{{ this.postData.username }}</div>
       <font-awesome-icon icon="ellipsis-h" class="icon-options"/>
+      <!-- <font-awesome-icon icon="heart" class="icon-options"/> -->
     </div>
 
     <!-- Posted image(s) -->
@@ -21,9 +22,18 @@
 
     <!-- Hashtags -->
     <div class="section-content-hashtags" v-if="hasHashtags()">
-      <div class="text hashtag" v-for="tag in this.postData.hashtags" :key="tag">
-        {{ "#" + tag }}
-      </div>
+      <div class="text hashtag" v-for="tag in this.postData.hashtags" :key="tag">{{ "#" + tag }}</div>
+    </div>
+
+    <!-- Post actions (like, comment) -->
+    <div class="section-actions">
+      <font-awesome-icon icon="heart" class="action"/>
+      <font-awesome-icon icon="comment" class="action"/>
+    </div>
+
+    <!-- Likes -->
+    <div class="section-likes">
+      <span class="text">Liked by bix_foxy04 and 13 others</span>
     </div>
 
     <!-- Post details (timestamp) -->
@@ -31,10 +41,10 @@
       <div class="text">{{ this.postData.timestamp }}</div>
     </div>
 
-    <!-- Post actions (like, comment) -->
-    <div class="section-actions">
-      <font-awesome-icon icon="heart" class="action"/>
-      <font-awesome-icon icon="comment" class="action"/>
+    <!-- Comments -->
+    <div class="section-comments">
+      <input class="text comments-text" placeholder="Say something..."/>
+      <div class="text comments-post">Post</div>
     </div>
   </div>
 </template>
@@ -56,8 +66,7 @@ export default {
       return require("@/assets/avatars/" + this.postData.avatar + ".png");
     },
     getImage(index) {
-      return require("@/assets/" +
-        this.postData.images[index]);
+      return require("@/assets/" + this.postData.images[index]);
     },
     // Returns true if this post contains user-posted images
     hasImages() {
@@ -88,11 +97,22 @@ export default {
   display: flex;
   flex-wrap: wrap;
   border-radius: 5px;
-  transition: .25s;
+  transition: 0.25s;
+  animation-name: example;
+  animation-duration: .4s;
+}
+
+@keyframes example {
+  from {
+    transform: scale(.7, .7);
+  }
+  to {
+    transform: scale(1, 1);
+  }
 }
 
 .post:hover {
-  box-shadow: 0px 5px 18px 0px rgba(79,79,79,0.1);
+  box-shadow: 0px 5px 18px 0px rgba(79, 79, 79, 0.1);
 }
 
 /* User */
@@ -161,7 +181,7 @@ export default {
   border-radius: 3px;
   margin: 5px 8px 3px 0;
   cursor: pointer;
-  transition: .15s
+  transition: 0.15s;
 }
 .section-content-hashtags > .hashtag:hover {
   background-color: rgb(174, 104, 221);
@@ -181,12 +201,11 @@ export default {
   width: 100%;
   padding: 0px 0px 0px 0px;
   overflow: hidden;
-  
 }
 
 /* About */
 .section-about > .text {
-  font-size: 13px;
+  font-size: 12px;
   color: rgb(182, 182, 182);
   padding: 7px 0px 0px 18px;
   display: inline-flex;
@@ -204,11 +223,48 @@ export default {
   font-size: 20px;
   display: inline-flex;
   color: rgb(218, 218, 218);
-  margin-right: 10px;
+  margin-right: 14px;
   transition: 0.15s;
 }
 .section-actions > .action:hover {
   color: rgb(221, 104, 104);
   cursor: pointer;
+}
+
+
+/* Likes */
+.section-likes {
+  width: 100%;
+  color: rgb(180, 180, 180);
+  cursor: default;
+  padding: 8px 0px 0px 18px;
+  display: flex;
+}
+
+/* Comments */
+.section-comments {
+  width: 100%;
+  padding: 10px 5px 0px 18px;
+  display: flex;
+  border-top: 1px solid rgb(236, 236, 236);
+  margin-top: 20px;
+  justify-content: space-between;
+}
+.section-comments > .comments-text {
+  width: 90%;
+  height: 30px;
+  background-color: white;
+  border: none;
+  color: grey;
+}
+.section-comments > .comments-post {
+  color: rgb(64, 100, 201);
+  padding: 5px 5px 5px 5px;
+  cursor: pointer;
+  opacity: .5;
+  transition: .15s;
+}
+.section-comments > .comments-post:hover {
+  opacity: 1;
 }
 </style>
